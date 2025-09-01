8b6b879d4cab5d8e668fa852d2390d5a1fa750a0
YAKUBU PACEEBA <yakubceeba@gmail.com>
2025-09-01 20:29:52 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -309,7 +309,7 @@ export default function HomePage() {
             </div>
           </ScrollAnimation>
 
-          <div className="grid md:grid-cols-4 lg:grid-cols-5 gap-8">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {[
               {
                 name: "Mr. Joseph Addai",
